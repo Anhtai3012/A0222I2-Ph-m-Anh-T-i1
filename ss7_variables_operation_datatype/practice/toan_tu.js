42a1edc616d613b836e12b571d4224eb3df725b0
unknown <mkhanhaa@gmail.com>
2022-03-16 14:01:51 +0700

    exercise

---
+++ ss7_variables_operation_datatype/practice/toan_tu.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+let inputwidth = prompt("enter the wifth");
+let inputheight=prompt("enter the height");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width*height;
+document.write("the area is" + area);
