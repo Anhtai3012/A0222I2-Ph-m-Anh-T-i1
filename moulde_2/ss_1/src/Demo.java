9e9a07bbf95c0d9058535bc07c237d5b5fb10fca
unknown <mkhanhaa@gmail.com>
2022-05-13 19:33:49 +0700

    demo

---
+++ moulde_2/ss_1/src/Demo.java
@@ -0,0 +1,48 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.printf("Hello word"+"123");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.printf("Hello word");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,");
+        System.out.println("swdfghjmk,dd");
+        System.out.printf("Hello word");
+    }
+}
